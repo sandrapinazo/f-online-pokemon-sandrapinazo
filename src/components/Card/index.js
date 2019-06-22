ddabcc4575e9c826e11bfc4950563e55686ee78d
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-06-22 20:48:40 +0200

    Add basic styles to pokemon card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,8 +5,9 @@ function Card(props) {
     const {id, types, name, img, evolves} = props;
     return(
       <div className='Pokemon'>
-        <img src={img} alt={name} />
+        <div className='PkmImg' style={{backgroundImage: `url(${img})`}} alt={name}>
         <p className='Id'>ID/{id}</p>
+        </div>
         <h2 className="Name">{name}</h2>
         <ul className="ListType">
           {types.map((item, index) => {
@@ -17,7 +18,9 @@ function Card(props) {
             );
           })}
         </ul>
-        {evolves !== 'none'? <p>Evolves from: {evolves}</p> : <p> </p> }
+        {evolves !== 'none'
+          ? <p className='Evolves'>Evolves from: <span className='Evolution'>{evolves}</span></p> 
+          : <div className='NoEvol'> </div> }
       </div>
     );
 }
